8e83be21dd2ad823e054ffd532e8e8bc2d8a5f11
kshubham175 <kshubham175@gmail.com>
2020-02-15 22:36:37 +0530

    Login

--- First.php
+++ First.php
@@ -12,35 +12,6 @@ if(@mysqli_query($dbc,$query))
 	else
 		print mysqli_error($dbc);
 ?>
-<DOCTYPE html>
-<html>
-<head>
- 
-</head>
 
-<meta charset="UTF-8">
-<link href="css/styles2.css" type="text/css" rel= "stylesheet" />
-
-<body>
-<h2>New Game!!</h2>
-<p>Welcome: 
-<?php
-print $name;
-?>
-</p>
-
-<canvas id="mycanvas" >
-</canvas>
-<button id="but">START
-</button>
-
- <script type= "text/javascript" src ="js/js2.js" >
- </script>
-</body>
-
-
-  
-	 
-</html>
 
  
