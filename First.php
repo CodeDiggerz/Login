7db0d68808ec2bf3552e23004b56434c7d2b6243
kshubham175 <kshubham175@gmail.com>
2020-02-15 23:09:56 +0530

    Login

--- First.php
+++ First.php
@@ -2,16 +2,92 @@
 $name=$_POST['name'];
 $userid=$_POST['userid'];
 $password=$_POST['Password'];
-$sex=$_POST['sex'];
 $dob=$_POST['dob'];
 setcookie('userid',$userid);
 $dbc=mysqli_connect('localhost','root','raina48','new');
-$query="insert into info(name,userid,password,sex,dob) values ('$name','$userid','$password','$sex','$dob')";
+$query="insert into info(name,userid,password,dob) values ('$name','$userid','$password','$dob')";
 if(@mysqli_query($dbc,$query))
-{ print "Congrats!! You have succefully registered.";}
+{ }
 	else
 		print mysqli_error($dbc);
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css" />
+    <!-- Gaanja -->
+  </head>
+
+  <body>
+    <div id="Head">
+      <header id="main-header">
+        <div class="container">
+          <h1 id="mainH">
+            <a href="#" id="Logo">Potatotiser</a>
+            <img id="TheLogo" src="images/logo.JPG" />
+          </h1>
+        </div>
+      </header>
+
+      <nav id="navbar">
+        <div class="container">
+          <ul>
+            <li style="background-color: gray;"><a href="#">Home</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Commitment</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">blog</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Log In</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <section id="showcase">
+      <div class="container">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        </h1>
+      </div>
+    </section>
+    <div class="container">
+      <section id="main">
+        <h1>Welcome
+			<?php
+			  print " ";
+			  print $name;
+			  ?>
+		</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+
+      <aside id="sidebar">
+        <h3 style="text-align: center;">Notice-Board</h3>
+        <ul>
+          <li>new report on price of fertilizers</li>
+          <li>crop reports : February 2020</li>
+          <li>How to keep soil ideal !</li>
+          <li>Kapila Pashu Ahar has launched Kapila Sampoorna</li>
+          <li>Buy brand new Powertrac Euro 50</li>
+          <li>weather today</li>
+        </ul>
+      </aside>
+    </div>
+
+    <footer id="main-footer">
+      <p>Copyright &copy; 2020 CodeDiggerz</p>
+    </footer>
+  </body>
+</html>
 
 
  
--- view.php
+++ view.php
@@ -5,7 +5,7 @@ if($username)
 {
 $password=$_POST['Password'];	
 
-$query='select userid,password,score,name from info';
+$query='select userid,password,name from info';
 $r=mysqli_query($dbc,$query);
 while($row=mysqli_fetch_array($r))
 {  
@@ -15,82 +15,158 @@ while($row=mysqli_fetch_array($r))
 		if($row['password']==$password)
 		{
 			setcookie('userid',$username);
-			print ' <!DOCTYPE html>
+      print '
+      <!DOCTYPE html>
 <html>
-<head>
- 
-</head>
+  <head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css" />
+    <!-- Gaanja -->
+  </head>
 
-<meta charset="UTF-8">
-<link href="css/styles2.css" type="text/css" rel= "stylesheet" />
+  <body>
+    <div id="Head">
+      <header id="main-header">
+        <div class="container">
+          <h1 id="mainH">
+            <a href="#" id="Logo">Potatotiser</a>
+            <img id="TheLogo" src="images/logo.JPG" />
+          </h1>
+        </div>
+      </header>
 
-<body>
-<h2>New Game!!</h2>
-<p>Welcome '; print $row['name'] ;print' </p>
-<p>Your Highest score: '; print $row['score'] ;print' </p>
-<canvas id="mycanvas" >
-</canvas>
-<button id="but">START
-</button>
+      <nav id="navbar">
+        <div class="container">
+          <ul>
+            <li style="background-color: gray;"><a href="#">Home</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Commitment</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">blog</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Log In</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <section id="showcase">
+      <div class="container">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        </h1>
+      </div>
+    </section>
+    <div class="container">
+      <section id="main">
+        <h1>Welcome 
+        ';
 
- <script type= "text/javascript" src ="js/js2.js" >
- </script>
-</body>
+			
+			  print " ";
+			  print $username;
+			  print '
+		</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
 
+      <aside id="sidebar">
+        <h3 style="text-align: center;">Notice-Board</h3>
+        <ul>
+          <li>new report on price of fertilizers</li>
+          <li>crop reports : February 2020</li>
+          <li>How to keep soil ideal !</li>
+          <li>Kapila Pashu Ahar has launched Kapila Sampoorna</li>
+          <li>Buy brand new Powertrac Euro 50</li>
+          <li>weather today</li>
+        </ul>
+      </aside>
+    </div>
 
-  
-	 
-</html> ';
+    <footer id="main-footer">
+      <p>Copyright &copy; 2020 CodeDiggerz</p>
+    </footer>
+  </body>
+</html>
+';
 
 	}
 		else 
 			print '
-		<!DOCTYPE html>
-<html>
-<title>LOGIN</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="css/new.css" ></link>
-<body>
-<div>
-  <form action="view.php" method ="POST">
-   <p>Registered User</p>
-   <br>
-   Username:<br>
-   <input type="text" name="username" placeholder="Enter Username" required >
-   <br>
-   Password:<br>
-   <input type="password" name="Password" placeholder="Enter Password" required >
-   <br>
-   <input type="submit" name="login" value="LOGIN">
-   <br>Incorrect Password
-    
-   </form>
-   <form action="first.php" method="POST" >
-   <p>New User</p>
-   <br>
-   Name:<br>
-   <input type="text" name="name" placeholder="Enter Name" required >
-   <br>
-   User Id:<br>
-   <input type="text" name="userid" placeholder ="Select UserId " required >
-   <br>
-   Password:<br>
-   <input type="password" name="Password" placeholder="Choose Password" required >
-   <br>
-   Sex:<br>
-   <select name="sex">
-   <option value="male">Male</option>
-   <option value="female">Female</option>
-   </select>
-   <br>
-   Date of Birth:<br>
-   <input type="date" name="dob">
-   <br>
-   <input type="submit" name="signup" value="SIGN UP">   
-  </form>
-  </div>
-</body>
-</html>';
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>My Website</title>
+          <link rel="stylesheet" type="text/css" href="css/login.css" />
+        </head>
+        <body>
+          
+          <div id="Head">
+          <header id="main-header">
+            <div class="container">
+              <h1 id="mainH"><a href="#" id="Logo">Potatotiser</a><img src="images/logo.JPG"></h1>
+            </div>
+          </header>
+      
+          <nav id="navbar">
+            <div class="container">
+              <ul>
+                <li ><a href="#">Home</a></li>
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Commitment</a></li>
+                <li><a href="#">Achievements</a></li>
+                <li><a href="#">blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li style="background-color: gray;"><a href="#">Log In</a></li>
+              </ul>
+            </div>
+          </nav>
+          </div> 
+          <div id="form">
+            <form action="view.php" method ="POST">
+             <p>Registered User</p>
+             
+             Username<br>
+             <input type="text" name="username" placeholder="Enter Username" required >
+             <br>
+             Password<br>
+             <input type="password" name="Password" placeholder="Enter Password" required >
+             <br>
+             <p> INCORRECT PASSWORD</p>
+             <input type="submit" name="login" value="LOGIN">
+              
+             </form>
+             <form action="first.php" method="POST" >
+             <p>New User</p>
+             
+             Name<br>
+             <input type="text" name="name" placeholder="Enter Name" required >
+             <br>
+             User Id<br>
+             <input type="text" name="userid" placeholder ="Select UserId " required >
+             <br>
+             Password<br>
+             <input type="password" name="Password" placeholder="Choose Password" required >
+             <br>
+             Date of Birth<br>
+             <input type="date" name="dob">
+             <br>
+             <input type="submit" name="signup" value="SIGN UP">   
+            </form>
+            </div>
+          <footer id="main-footer">
+            <p>Copyright &copy; 2020 CodeDiggerz</p>
+          </footer>
+        </body>
+      </html>
+      ';
 	}
 }
 
